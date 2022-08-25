965d7ccb0f88d950157f70a3542040388f4c2b49
Steven Wang 王先誠 <stevenwang@ieinet.org>
2022-08-25 18:12:51 +0800

    day2

--- src/components/day2/Day2.js
+++ src/components/day2/Day2.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./day2.css";
 
 export default function Day2() {
   let navigate = useNavigate();
   return (
     <div>
-      <p>Day2</p>
+      <div class="frame">
+        <div class="center">
+          <div class="menu-icon">
+            <div class="line-1 no-animation"></div>
+            <div class="line-2 no-animation"></div>
+            <div class="line-3 no-animation"></div>
+          </div>
+        </div>
+      </div>
       <button onClick={() => navigate("/")}>Back to home!</button>
     </div>
   );
